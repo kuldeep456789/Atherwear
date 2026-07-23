4f2b759c4298676d60b0aeb8e0b4ade893f1e67d
kuldeep456789 <kuldeep456789>
2026-07-23 16:53:33 +0530

    Position all popup toast messages at the bottom of the screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <ThemeProvider>
         <App />
         <Toaster
-          position="top-right"
+          position="bottom-center"
           toastOptions={{
             duration: 3000,
             style: {
